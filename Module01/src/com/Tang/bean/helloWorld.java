bc5db7ec17f0976cd074579171d5c7e976b39b32
970640814 <970640814@qq.com>
2021-11-03 22:35:49 +0800

    Initial commit

---
+++ Module01/src/com/Tang/bean/helloWorld.java
@@ -0,0 +1,15 @@
+package com.Tang.bean;
+
+public class helloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("helloWorld.main");
+        //模板一：sout/soutm/soutv/soutp
+        //模板二：fori/iter/itar
+        //模板三：list.for/list.fori/list.forr
+        //模板四：ifn/inn/xxx.nn/xxx.null
+        //模板五：prsf：private static final/psf:public static final/psfi/psfs
+    }
+
+}
